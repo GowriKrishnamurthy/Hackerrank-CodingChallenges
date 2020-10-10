a7e137c997fce3bdd6e42e33ba434fc746337ed8
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-10-10 22:47:12 +1100

    Create LongestWord.cs

---
+++ LongestWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+/*
+ * Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. 
+ * If there are two or more words that are the same length, return the first word from the string with that length. 
+ * Ignore punctuation and assume sen will not be empty.
+
+Examples
+Input: "fun&!! time"
+Output: time
+
+Input: "I love dogs"
+Output: love
+ * */
+namespace CodingChallenges
+{
+    class MainClass
+    {
+        public static string LongestWord(string sen)
+        {
+            // Match all characters that DO NOT(^) match(\w alphanumeric characters) and \s(white space and tab) 
+            // and replace them with ""
+            Regex regx = new Regex(@"[^\w\s]");
+            sen = regx.Replace(sen, "");
+            string[] words = sen.Split(" ");
+            return words.OrderByDescending(x => x.Length).First();
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(LongestWord(Console.ReadLine()));
+        }
+    }
+}
