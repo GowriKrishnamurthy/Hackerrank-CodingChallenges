efa27ba67d0c6ed9fc9b7fe8e4144815f9741d9b
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-10-10 16:33:15 +1100

    Reverse array implementation

---
+++ ReverseArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Solution
+{
+    static int[] reverseArray(int[] a)
+    {
+        int length = a.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            int swap = a[i];
+            a[i] = a[length - i - 1];
+            a[length - i - 1] = swap;
+        }
+        return a;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        int[] res = reverseArray(arr);
+
+        Console.WriteLine(string.Join(" ", res));
+    }
+}
