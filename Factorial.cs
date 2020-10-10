b5c9e678c9f5e18e5e6ff61f05f2219dae8ffd98
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-10-10 23:00:13 +1100

    Create Factorial.cs

---
+++ Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it.
+For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. 
+For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+ * */
+namespace CodingChallenges
+{
+    class MainClass
+    {
+        public static int Factorial1(int num)
+        {
+            if (num == 1)
+                return 1;
+            else
+                return num * Factorial1(num - 1);
+        }
+        public static int Factorial2(int num)
+        {
+            for (int i = num - 1; i > 0; i--)
+                num = num * i;
+         
+            return num;
+        }
+
+        static void Main()
+        {
+            int.TryParse(Console.ReadLine(), out int num);
+            Console.WriteLine(Factorial2(num));
+        }
+    }
+}
