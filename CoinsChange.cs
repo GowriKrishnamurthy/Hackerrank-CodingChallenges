ec01247e8108356f5d6f04cc7f520d14c2ccccd1
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-10-11 11:41:23 +1100

    Create CoinsChange.cs

---
+++ CoinsChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class MainClass
+{
+    public static string CoinsChange(int amount)
+    {
+        List<int> coinsDenominations = new List<int> { 500, 200, 100, 50};
+        string result = string.Empty;
+
+        foreach (var coin in coinsDenominations)
+        {
+            int coinsUsed = amount / coin;
+            amount = amount - (coinsUsed * coin);
+            
+            if(coinsUsed > 0)
+                result += $"{coin} coins used: {coinsUsed}. ";
+        }
+
+        return result.TrimEnd('.') + $" and remaining amount: {amount}";
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter amount");
+        int.TryParse(Console.ReadLine(), out int amount);
+        if(amount > 0)
+            Console.WriteLine(CoinsChange(amount));
+    }     
+}
